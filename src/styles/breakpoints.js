56a7058a14ad228dfd055018e6a0f916fb96bd64
Kreal11 <kirillshyrokov2@gmail.com>
2024-03-22 16:25:35 +0200

    added base structure for project + reseted styles

---
+++ src/styles/breakpoints.js
@@ -0,0 +1,11 @@
+export const breakpoints = {
+  mobile: "375px",
+  tablet: "768px",
+  desktop: "1440px",
+};
+
+export const breakpointsNumbers = {
+  mobile: 375,
+  tablet: 768,
+  desktop: 1440,
+};
